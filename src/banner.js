4adbe45fe4da91bc91743a35d3d2a113a168717c
Smart-Dev719 <blockchaindev719@gmail.com>
2022-03-16 07:45:55 -0400

    last update

--- src/banner.js
+++ src/banner.js
@@ -8,34 +8,36 @@ import { FaPlus, FaMinus } from "react-icons/fa";
 import swal from 'sweetalert';
 
 const Banner = (props) => {
-  const {
-    preLoading,
-    setPreLoading,
-    account,
-    metamaskConnected,
-    connectToMetamask,
-  } = props;
-  const [selectedCount, setSelectedCount] = useState(1);
+  const { metamaskConnected, account, setMetamaskConnnected } = props;
+
   const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
+  const [preLoading, setPreLoading] = useState(false);
+  const [selectedCount, setSelectedCount] = useState(1);
   const mintable = useSelector((state) => state.mint);
   const [mintLoading, setMintLoading] = useState(false);
   const [mintStatus, setMintStatus] = useState(false);
   const [viewModal, setViewModal] = useState(false);
-  // const [networkId, setNetworkId] = useState();
+  const [networkId, setNetworkId] = useState();
   const [modal, setModal] = useState(false);
 
-  // useEffect(async () => {
-  //   const web3 = window.web3;
-  //   const networkId = await web3.eth.net.getId();
-  //   setNetworkId(networkId);
-  //   window.ethereum.on("networkChanged", (networkId) => {
-  //     setNetworkId(networkId);
-  //   });
-  // });
 
-  useEffect(async () => {
+  const handleConnectWallet = async () => {
+    if (window.ethereum) {
+      await window.ethereum.enable();
+      setMetamaskConnnected(true);
+      window.location.reload();
+    }
+  };
+
+  useEffect(() => {
     if (account) {
-      dispatch(onCheckMintable({ address: account }));
+      setPreLoading(true);
+      dispatch(
+        onCheckMintable({
+          address: account,
+        })
+      );
     }
   }, [account]);
 
@@ -98,34 +100,40 @@ const Banner = (props) => {
           className="ArrowIcon rightIcon"
           onClick={() => {
             console.log("plus");
-            // if (selectedCount < mintable.count) {
-            //   setSelectedCount(selectedCount + 1);
-            // }
-            if (account) {
-              if (selectedCount < mintable.count) {
-                setSelectedCount(selectedCount + 1);
-              }
-            } else {
-              swal("Sorry!", "Please connect wallet, first!", "warning");
+            if (selectedCount < 10) {
+              setSelectedCount(selectedCount + 1);
             }
+            // if (account) {
+            //   if (selectedCount < mintable.count) {
+            //     setSelectedCount(selectedCount + 1);
+            //   }
+            // } else {
+            //   swal("Sorry!", "Please connect wallet, first!", "warning");
+            // }
           }}
         >
           <FaPlus />
         </span>
       </div>
       {/* <button onClick={connectToMetamask}>Connect wallet</button> */}
-      {metamaskConnected ? (
-        mintable.count > 0 ? (
-          <button className="MintBtn" onClick={handleMint}>
-            MINT YOUR NAVIKATZ NFTS!
-          </button>
-        ) : (
-          <button className="MintBtn disable">
-            MINT YOUR NAVIKATZ NFTS!
-          </button>
-        )
+      {metamaskConnected && account ? (
+        // mintable.count > 0 ? (
+        //   <button className="MintBtn" onClick={handleMint}>
+        //     MINT YOUR NAVIKATZ NFTS!
+        //   </button>
+        // ) : (
+        //   <button className="MintBtn disable">
+        //     MINT YOUR NAVIKATZ NFTS!
+        //   </button>
+        // )
+        <button className="MintBtn" onClick={handleMint}>
+          MINT YOUR NAVIKATZ NFTS!
+        </button>
       ) : (
-        <button className="MintBtn" onClick={connectToMetamask}>
+        <button className="MintBtn"
+          onClick={() => {
+            handleConnectWallet();
+          }}>
           CONNECT WALLET
         </button>
       )}
